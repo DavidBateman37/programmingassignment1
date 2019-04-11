28c93554f2a218c5440c2ead7b795019b12c178f
David Bateman <david.bateman@uon.edu.au>
2019-04-11 04:47:06 +0000

    Added a string function with the alphabet in it

---
+++ main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+char encrypt[27]= {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'Y', 'Z', '\0'};
+int main(){
+    
+    return 0;
+}
