45f2a7341caf0285f1aa40a8fe25dd0e2730f7da
David Bateman <david.bateman@uon.edu.au>
2019-04-11 05:25:17 +0000

    Added a lot of stuff (probs wrong)

--- main.c
+++ main.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
 
-char encrypt[27]= {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'Y', 'Z', '\0'};
+
 int main(){
     
+char message[100];
+char alphabet[26]= {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'Y', 'Z'};
+int key;
+int i;
+
+for (i=0; message[i]!= '\0'; i++)
+{ 
+   letter = message[i];
+   
+   if (letter >= 'A' && letter <= 'Z')
+   {
+       letter = letter + key;
+       message[i] = letter;
+            
+   }
+   else {
+       printf("Error");
+   }
+     
+}
+
+    printf{"Encrypted message; %s", letter};
+
     return 0;
 }
