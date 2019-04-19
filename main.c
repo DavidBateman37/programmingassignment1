6a4fba15effb85f8c60123160d56200787774d85
David Bateman <david.bateman@uon.edu.au>
2019-04-19 07:46:18 +0000

    100% working (I think)

--- main.c
+++ main.c
@@ -8,21 +8,23 @@ char message[100];
 char alphabet[27]= {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'Y', 'Z', '\0'};
 int key;
 int i;
+int lk;
 
 printf("Enter a message:\n");
 scanf("%s", message);
 printf("Enter a key:\n");
 scanf("%d", &key); 
 for (i=0; message[i]!= '\0'; i++)
-{ letter= message[i];
+{   letter= message[i];
+    lk= letter + key;
    
-   if (letter >= 'A' && letter <= 'Z')
+   if (lk >= 65 && lk <= 90)
    {
-       letter = letter + key;
+       letter = lk;
             
    }
-   else if (letter > 'Z') {
-       letter = letter - 'Z' + 'A' -1;
+   else if (lk > 90) {
+       letter = lk - 26;
        
    }
        message[i]= letter;
