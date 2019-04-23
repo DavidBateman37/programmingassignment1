e2bc7161d883d53356d0c46cb479df19bc0525e1
David Bateman <david.bateman@uon.edu.au>
2019-04-23 07:29:19 +0000

    lowercase letters now work, still dont have whitespaces working

--- main.c
+++ main.c
@@ -1,51 +1,42 @@
 #include <stdio.h>
+#include <string.h>
 
 void rotationEncrypt(char*message, int key);
 void rotationDecrypt(char*message, int key);
 int main(){
     
-char letter;
 char message[100];
 char alphabet[27]= {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'Y', 'Z', '\0'};
 int key;
 int i;
-int lk;
 int test;
 
 printf("Select a test by entering a number:\n");
 printf("1: Rotation cipher encryption with a key\n");
 printf("2: Rotation cipher decryption with a key\n");
 printf("Choose a test: \n");
-scanf("%d", test);
-if (test>=1, test<=6){
-    scanf("%d", &test);
-    printf("Your selection: %d\n", test);
-}
+scanf("%d", &test);
 
-else {
-    printf("Error, please select a test");
-    return 0;
-}
 
 switch(test){
     
     case 1:
-rotationEncrypt(message, key);   
 printf("Enter a message:\n");
-scanf("%s", message);
+scanf("%s", &message);
 printf("Enter a key:\n");
 scanf("%d", &key); 
+rotationEncrypt(message, key);
 
 
     printf("Decrypted message: %s\n", message);
     break;
     
     case 2:
-rotationalDecrypt(message, key);
 printf("Enter a message:\n");
 scanf("%s", message);
 printf("Enter a key:\n");
 scanf("%d", &key); 
+rotationDecrypt(message, key);
 
 
     printf("Encrypted message: %s\n", message);
@@ -54,12 +45,17 @@ scanf("%d", &key);
 }
 }
 
-void rotationEncrypt(char*message, int key){
+void rotationEncrypt(char *message, int key){
     
  int i;
  int lk;
+ char letter;
  for (i=0; message[i]!= '\0'; i++)
 {   letter= message[i];
+if (letter >= 96 && letter <= 122)
+        {
+            letter = letter - 32;
+        }
     lk= letter + key;
    
    if (lk >= 65 && lk <= 90)
@@ -75,12 +71,17 @@ void rotationEncrypt(char*message, int key){
 }
 }
 
-void rotationalDecrypt(char*message, int key){
+void rotationDecrypt(char*message, int key){
     
     int i;
-    in lk;
+    int lk;
+    char letter;
     for (i=0; message[i]!= '\0'; i++)
 {   letter= message[i];
+if (letter >= 96 && letter <= 122)
+        {
+            letter = letter - 32;
+        }
     lk= letter - key;
    
    if (lk >= 65 && lk <= 90)
